3bfa6608c1c776439cf19619f011b59b16943d66
Angelo Polo <language.devel@gmail.com>
2020-11-04 23:12:51 +0100

    New output format: returns bins, oversized, and invalid. Converts object inputs to arrays.

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # bin-packer v1.0.0
 
-Pack an object's properties into an array of uniform sized bins.
+Packs objects into bins of a specified maximum size.
 
-Each property of the object must have a uniformly named numeric sub-property giving the property's "size". The size value must be a number: values encoded as strings must be converted prior to passing to one of the algorithms.
+Accepts either an array of objects or an ordinary object with object-valued properties. In the later case, the object's property keys are discarded before packing the property values, so if the keys are to be preserved (e.g. by storing on the values) this should be done prior to calling a bin packing algorithm.
+Each value must have a uniformly named numeric sub-property giving the property's "size". Values with non-numeric sizes are returned as invalid (see Output). In particular, sizes encoded as strings must be converted beforehand.
 
 ## Install
 
@@ -15,96 +16,107 @@ npm install bin-packer
 ### Methods
 
 #### Arguments
-- `obj`: The object whose own enumerable properties are to be binned.
+- `obj`: The array or object whose own enumerable properties are to be binned.
 - `measure`: The `string` name of the sub-property giving the size to bin on.
 - `max`: The maximum bin size.
 
 #### Output
 Each algorithm returns an object with the following keys:
-- `bins`: An array of objects each with total size less than or equal to `max`.
-- `oversized`: An object containing any properties which alone have a size greather than `max`.
-- `invalid`: An object containing any properties with a non-object value or with a missing numeric `measure` property.
+- `bins`: An array of arrays, each with total size less than or equal to `max`.
+- `oversized`: An array containing any values which on their own have a size greather than `max`.
+- `invalid`: An array containing any values with a non-object value or lacking the numeric `measure` property.
 
 ### Algorithms
 
 #### nextFit(obj, measure, max)
-Naive algorithm: Opens a new bin whenever a file doesn't fit in the latest one.
+Opens a new bin whenever a value doesn't fit in the latest one.
 
 #### firstFit(obj, measure, max)
-Better: Tries to fit new items in all opened bins before opening a new one.
+Tries to fit new items in all opened bins before opening a new one.
 
 #### firstFitDecreasing(obj, measure, max)
-Even better: Runs a sort first so the hardest to place items are put down first.
+Runs a sort first (so the hardest to place items are put down first), then uses firstFit.
 
 ### Utility method
 
-#### quicksortObj(obj, measure)
-Sorts the keys of `obj` on the value of the numeric property given by the string `measure`. An array of single-key objects is returned.
+#### quicksort(obj, measure, lowToHigh=true)
+Sorts values by their `measure` property value. `lowToHigh` controls whether the result is sorted from smallest to largest or largest to smallest.
 
 
 ## Example
-Some JSON input:
+Example JSON input:
 ```json
-{ "0": { "size": 58,
-         "att": 0.35 },
-  "1": { "size": 36,
-         "att": 0.33 },
-  "2": { "size": 31,
-         "att": 0.49 },
-  "3": { "size": 7,
-         "att": 0.53 },
-  "4": { "size": 76,
-         "att": 0.25 },
-  "5": { "size": 47,
-         "att": 0.07 },
-  "etc": "..."
-}
+[ { "size": 3.08,
+    "label": "dolore"
+  },
+  { "size": 7.89,
+    "label": "nulla"
+  },
+  { "size": 44.51,      // Larger than max
+    "label": "nostrud"
+  },
+  { "size": 6.62,
+    "label": "proident"
+  },
+  { "size": "8.05",     // Invalid non-numeric size
+    "label": "in"
+  },
+  //...
+  { "size": 5.56,
+    "label": "nisi"
+  }
+]
 ```
 Pack it into bins:
 ```js
-const binPacker = require('bin-packer')
-//, data = ...
-  , max = 300
-  , measure = 'size'
-  , result = binPacker.firstFitDecreasing(JSON.parse(data), measure, max)
+const binPacker = require('../src/lib/bin-packer')
+  //, data = JSON.parse(...)
+    , result = binPacker.firstFitDecreasing(data, 'size', 10)
 
-console.log(result.bins)
+console.log("Bins: %O", result.bins)
+console.log("Oversized: %O", result.oversized)
+console.log("Invalid: %O", result.invalid)
 ```
 Results in an array of bins:
 ```js
-[ { '7': { size: 90, att: 0.99 },
-    '10': { size: 10, att: 0.35 },
-    '12': { size: 90, att: 0.43 },
-    '14': { size: 8, att: 0.08 },
-    '16': { size: 1, att: 0.43 },
-    '24': { size: 98, att: 0.72 } },
-  { '5': { size: 47, att: 0.07 },
-    '6': { size: 83, att: 0.21 },
-    '8': { size: 87, att: 0.39 },
-    '11': { size: 81, att: 0.63 } },
-  { '4': { size: 76, att: 0.25 },
-    '15': { size: 73, att: 0.69 },
-    '17': { size: 76, att: 0.4 },
-    '22': { size: 75, att: 0.76 } },
-//    ...
+Bins: [
+  [
+    { size: 7.89, label: 'nulla' },
+    { size: 0.79, label: 'consectetur' },
+    { size: 0.13, label: 'fugiat' }
+  ],
+  [
+    { size: 6.62, label: 'proident' },
+    { size: 3.08, label: 'dolore' }
+  ],
+  [
+    { size: 5.56, label: 'nisi' },
+    { size: 2.88, label: 'eiusmod' }
+  ],
+  [ { size: 2.18, label: 'occaecat' } ]
 ]
+Oversized: [ { size: 44.51, label: 'nostrud' } ]
+Invalid: [ { size: '8.05', label: 'in' } ]
 ```
 
 Using the quicksort utility:
 ```js
-const result = binPacker.quicksortObj(data, measure)
-console.log(result.sorted)
+const sorted = binPacker.quicksort(data, measure)
+console.log('Sorted: %O', sorted.sorted)
+console.log("Invalid: %O", result.invalid)
 ```
 Results in a sorted array of single-key objects:
 ```js
-[ { '16': { size: 1, att: 0.43 } },
-  { '3': { size: 7, att: 0.53 } },
-  { '14': { size: 8, att: 0.08 } },
-  { '10': { size: 10, att: 0.35 } },
-  { '20': { size: 25, att: 0.98 } },
-  { '2': { size: 31, att: 0.49 } },
-  { '1': { size: 36, att: 0.33 } },
-  { '23': { size: 39, att: 0.23 } },
-//    ...
+Sorted: [
+  { size: 0.13, label: 'fugiat' },
+  { size: 0.79, label: 'consectetur' },
+  { size: 2.18, label: 'occaecat' },
+  { size: 2.88, label: 'eiusmod' },
+  { size: 3.08, label: 'dolore' },
+  { size: 5.56, label: 'nisi' },
+  { size: 6.62, label: 'proident' },
+  { size: 7.89, label: 'nulla' },
+  { size: 44.51, label: 'nostrud' }
 ]
+Invalid: [ { size: '8.05', label: 'in' } ]
 ```
--- spec/bin-packer-spec.js
+++ spec/bin-packer-spec.js
@@ -2,14 +2,64 @@
 'use strict'
 
 const fs = require('fs')
-  , dataFilePath = './spec/data/data_04.json'
-  , data = JSON.parse(fs.readFileSync(dataFilePath))
-  , binPacker = require('../lib/bin-packer')
-  , quicksortObj = require('../lib/util/quicksort-obj')
-  , measure = 'size'
-  //, measure = 'att'
-  , max = 32.7
-  , oversizedInData = true
+    , dataFilePath = './spec/data/data_04.json'
+    , data = JSON.parse(fs.readFileSync(dataFilePath))
+    , binPacker = require('../lib/bin-packer')
+    , quicksort = require('../lib/util/quicksort')
+    , utils = require('../lib/util/utils')
+    , measure = 'size'
+    //, measure = 'att'
+    , max = 32.7
+    , oversizedInData = true
+    , obj1 = { '43': {
+          size: 2,
+          sizeDecimal: 2.15,
+          guid: '10324ecc-fcfa-4bc2-84b9-e595155a5b50',
+          label: 'do'
+        },
+        '82': {
+          size: 30,
+          sizeDecimal: 30.15,
+          guid: 'ec7bd17e-1d85-475d-ab36-416dae8f18f4',
+          label: 'elit'
+        },
+        '12': {
+          size: 3,
+          sizeDecimal: 3.15,
+          guid: 'bd72cb34-3801-4e9b-b41d-818dbfe1e88e',
+          label: 'aute'
+        },
+        '96': {
+          size: 28,
+          sizeDecimal: 28.15,
+          guid: '1e2a22b6-c95d-4359-82d2-d5d6d807de61',
+          label: 'sit'
+        },
+        '41': {
+          size: 24,
+          sizeDecimal: 24.15,
+          guid: '8f638ee5-c573-4f00-bc79-57c41318e1a0',
+          label: 'minim'
+        },
+        '42': {
+          size: 7,
+          sizeDecimal: 7.15,
+          guid: 'ba524759-4831-42e6-acf2-5c701194900e',
+          label: 'non'
+        },
+        '73': {
+          size: 24,
+          sizeDecimal: 24.15,
+          guid: '0fb8e8ee-203b-4754-bdf8-2349558bb17a',
+          label: 'consequat'
+        },
+        '86': {
+          size: 7,
+          sizeDecimal: 7.15,
+          guid: '2ef4822d-c461-488c-887b-43712383698b',
+          label: 'anim'
+        }
+      }
 
 function anyTooBig(testBins, maximum) {
   for (const i in testBins) {
@@ -197,68 +247,81 @@ describe('bin-packer', function () {
   })
 })
 
-describe('quicksort-obj', function () {
+describe('quicksort', function () {
   describe('getMedianOfThree', function () {
-    const perms = [ [{'0': {idem: 0}}, {'1': {idem: 1}}, {'2': {idem: 2}}],
-                  [{'0': {idem: 0}}, {'2': {idem: 2}}, {'1': {idem: 1}}],
-                  [{'1': {idem: 1}}, {'0': {idem: 0}}, {'2': {idem: 2}}],
-                  [{'1': {idem: 1}}, {'2': {idem: 2}}, {'0': {idem: 0}}],
-                  [{'2': {idem: 2}}, {'0': {idem: 0}}, {'1': {idem: 1}}],
-                  [{'2': {idem: 2}}, {'1': {idem: 1}}, {'0': {idem: 0}}] ]
-      , correctAnswers = [1, 2, 0, 0, 2, 1]
+    const perms = [ [{idem: 0}, {idem: 1}, {idem: 2}],
+                  [{idem: 0}, {idem: 2}, {idem: 1}],
+                  [{idem: 1}, {idem: 0}, {idem: 2}],
+                  [{idem: 1}, {idem: 2}, {idem: 0}],
+                  [{idem: 2}, {idem: 0}, {idem: 1}],
+                  [{idem: 2}, {idem: 1}, {idem: 0}] ]
+        , correctAnswers = [1, 2, 0, 0, 2, 1]
 
-    it('should find the location of 1 each time', function () {
+    it('should find the location of 1', function () {
       for (const i in perms) {
-        const foundMedian = quicksortObj._getMedianOfThree(perms[i], 'idem', 0, perms[i].length - 1)
+        const foundMedian = quicksort._getMedianOfThree(perms[i], 'idem', 0, perms[i].length - 1)
         expect(foundMedian).toEqual(correctAnswers[i])
       }
     })
   })
 
-  describe('getArrayOfObjSingletons', function () {
-    const {singletons: array} = quicksortObj._getArrayOfObjSingletons(data)
-
-    it('should have length equal to number of keys in data', function () {
-      expect(array.length).toEqual(Object.keys(data).length)
-    })
-
-    it('should contain an object in each cell', function () {
-      let failedType = false
-
-      for (const i in array) {
-        if (Object.prototype.toString.call(array[i]) !== '[object Object]') {
-          failedType = true
-          break
+  describe('quicksort', function () {
+    it('should not sort a larger value before a smaller value', function () {
+      const {sorted: sorted} = quicksort.quicksort(data, measure)
+      expect(sorted.length).toEqual(data.length)
+      for (let i = 0; i < sorted.length - 1; ++i) {
+        if (sorted[i][measure] > sorted[i + 1][measure]) {
+          fail(`size ${sorted[i][measure]} at index ${i} > ${sorted[i + 1][measure]} at index ${i + 1}`)
         }
       }
-      expect(failedType).toBeFalsy()
     })
 
-    it('should contain an exactly one key in each cell', function () {
-      let failedCount = false
-
-      for (const i in array) {
-        if (Object.keys(array[i]).length !== 1) {
-          failedCount = true
-          break
+    it('should not sort a smaller value before a larger value', function () {
+      const {sorted: sorted} = quicksort.quicksort(data, measure, false)
+      expect(sorted.length).toEqual(data.length)
+      for (let i = 0; i < sorted.length - 1; ++i) {
+        if (sorted[i][measure] < sorted[i + 1][measure]) {
+          fail(`size ${sorted[i][measure]} at index ${i} < ${sorted[i + 1][measure]} at index ${i + 1}`)
         }
       }
-      expect(failedCount).toBeFalsy()
+    })
+  })
+})
+
+describe('utils', function () {
+  describe('toValuesArray', function () {
+    it('should return arrays unchanged', function () {
+      const array = [3, 9, 'Sunday', {'fruit': 'grape', 'grams': 500.4}, 86.144]
+          , originalLength = array.length
+          , processed = utils.toValuesArray(array)
+      expect(processed === array).toBeTrue()
+      expect(processed.length).toEqual(originalLength)
+    })
+    it('should return object\'s values', function () {
+      const originalSize = Object.keys(obj1).length
+          , processed = utils.toValuesArray(obj1)
+      expect(Array.isArray(obj1)).toBeFalse()
+      expect(Array.isArray(processed)).toBeTrue()
+      expect(processed.length).toEqual(originalSize)
     })
   })
 
-  describe('quicksortObj', function () {
-
-    it('should not sort a larger value before a smaller value', function () {
-      const {sorted: sorted} = quicksortObj.quicksortObj(data, measure)
-      let outOfPlace = false
-      for (let i = 0; i < sorted.length - 1; ++i) {
-        if (sorted[i] > sorted[i + 1]) {
-          outOfPlace = true
-          break
-        }
-      }
-      expect(outOfPlace).toBeFalsy()
+  describe('validate', function () {
+    const array = [3, 9, 'Sunday', {'fruit': 'grape', 'grams': 500.4}, {'fruit': 'orange', 'grams': 1200}, 86.144]
+    it('should should return an object with valid and invalid keys', function () {
+      const result = utils.validate(array, obj => true)
+      expect(result.valid).not.toBeUndefined()
+      expect(result.invalid).not.toBeUndefined()
+      expect(result.something).toBeUndefined()
+      expect(result.valid.length).toEqual(array.length)
+      expect(result.invalid.length).toEqual(0)
+    })
+    it('should find objects with a numeric \'grams\' property', function () {
+      const result = utils.validate(array, utils.measureValidator.bind(null, 'grams'))
+      expect(result.valid.length).toEqual(2)
+      expect(result.invalid.length).toEqual(4)
+      expect(result.valid[0].fruit).toEqual('grape')
+      expect(result.invalid[result.invalid.length -1]).toEqual(86.144)
     })
   })
 })
