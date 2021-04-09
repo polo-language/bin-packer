2efc94e25b91565c96c48a5ed51aa6f3050af3c9
Angelo Polo <language.devel@gmail.com>
2021-04-09 10:07:33 +0200

    Validate bin invariants

--- src/validation.ts
+++ src/validation.ts
@@ -1,53 +1,72 @@
 import { Item, Bin, Move, ChangeReport }  from './common'
 
+interface ErrorHandler {
+  handle: (message: string) => void
+}
+
+class ThrowingErrorHandler implements ErrorHandler {
+  handle(message: string) {
+    throw new Error(message)
+  }
+}
+
 /**
- * Throws if any items are missing/added between the two inputs.
- * Throws if any item still has an undefined newBinId.
- * Throws if any bins have been added or removed.
- * @param beforeBins 
- * @param afterBins 
+ * Checks whether any items are missing/added between the two inputs.
+ * Checks whether any item still has an undefined newBinId.
+ * Checks whether any bins have been added or removed.
  */
 export function itemAccounting(
     beforeBins: readonly Bin[],
     newItems: readonly Item[],
     afterBins: Bin[]) {
+  const errorHandler = new ThrowingErrorHandler()
   if (beforeBins.length !== afterBins.length) {
-    throw new Error(`Started with ${beforeBins.length} bins, ended up with ${afterBins.length} ` +
-        'bins')
+    errorHandler.handle(`Started with ${beforeBins.length} bins, ended up with ` +
+        `${afterBins.length} bins`)
   }
   const beforeBinIds = new Set(beforeBins.map(bin => bin.id))
   if (beforeBinIds.size !== beforeBins.length) {
-    throw new Error(`Non-unique IDs present in original bins`)
+    errorHandler.handle('Non-unique IDs present in original bins')
   }
   const beforeItemIds = new Set(beforeBins.flatMap(bin => bin.items.map(item => item.id)))
   newItems.forEach(item => beforeItemIds.add(item.id))
   let newItemCount = 0
   for (const bin of afterBins) {
     if (!beforeBinIds.has(bin.id)) {
-      throw new Error(`Bin with ID ${bin.id} was not present in the original data`)
+      errorHandler.handle(`Bin with ID ${bin.id} was not present in the original data`)
     }
     for (const item of bin.items) {
       ++newItemCount
       if (!beforeItemIds.has(item.id)) {
-        throw new Error(`Item with ID ${item.id} was not present in the original data`)
+        errorHandler.handle(`Item with ID ${item.id} was not present in the original data`)
       }
       if (item.newBinId === undefined) {
-        throw new Error(`Item with ID ${item.id} and original bin ID ${item.originalBinId} has ` +
-            'not been assigned a new bin ID')
+        errorHandler.handle(`Item with ID ${item.id} and original bin ID ${item.originalBinId} ` +
+            'has not been assigned a new bin ID')
       }
     }
   }
   if (beforeItemIds.size !== newItemCount) {
-    throw new Error(`There were ${beforeItemIds.size} items before, but ${newItemCount} afterwards`)
+    errorHandler.handle(`There were ${beforeItemIds.size} items before, but ${newItemCount} ` +
+        'afterwards')
   }
 }
 
 /**
- * Throws if any bin invariants are violated.
- * @param bins 
+ * Checks whether any bin invariants are violated.
  */
 export function validateBins(bins: Bin[]) {
-  // TODO
+  const errorHandler = new ThrowingErrorHandler()
+  for (const bin of bins) {
+    if (bin.itemCount > bin.maxItems) {
+      errorHandler.handle(`Bin ${bin.id} with max items ${bin.maxItems} contains ` +
+          `${bin.itemCount} items`)
+    }
+    if (bin.utilization > bin.capacity) {
+      errorHandler.handle(`Bin ${bin.id} with capacity ${bin.capacity} contains items with ` +
+          `${bin.utilization} total utilization`)
+    }
+  }
 }
 
 /**
